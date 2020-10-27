f0d0b4d81c04465f4f6f0e2e4febfc49694ba18b
coroo <coroo.wicaksono@gmail.com>
2020-10-27 15:34:53 +0700

    :sparkles: initial commit

---
+++ app/index.ts
@@ -0,0 +1,41 @@
+import * as fastify from 'fastify';
+import mongoose from 'mongoose';
+import routes from './routes';
+import { Options } from './config/swagger';
+import { config } from './config';
+import swagger from 'fastify-swagger';
+const env = process.env.NODE_ENV;
+
+// Configure HTTP server
+const app = fastify.default({ logger: true });
+
+// Register Swagger
+app.register(swagger, Options);
+
+routes.forEach(route => {
+	app.route(route);
+});
+
+const start = async (): Promise<void> => {
+	try {
+		await app.listen(config.app.port);
+		app.swagger();
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+};
+start();
+
+export default app;
+
+// Configure DB
+if (env !== 'test') {
+	mongoose
+		.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		.then(() => app.log.info('MongoDB connected...'))
+		.catch(err => app.log.error(err));
+}
